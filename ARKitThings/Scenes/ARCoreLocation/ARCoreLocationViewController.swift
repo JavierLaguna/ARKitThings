73da70a6eba196cd3c03014b5475ef9af383c88a
Javier Laguna <javier.laguna@innocv.com>
2024-09-19 12:12:29 +0200

    Create ARCoreLocationViewController file and add arkit-corelocation dependency

---
+++ ARKitThings/Scenes/ARCoreLocation/ARCoreLocationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ARCoreLocationViewController.swift
+//  ARKitThings
+//
+//  Created by Javier Laguna on 6/9/24.
+//
+
+import UIKit
+
+class ARCoreLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
